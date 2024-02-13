28fa4d3e9cb528fbb93a65775da1621a5e8e2e34
JoelYeet <joelyeeyongyi99@gmail.com>
2024-02-13 13:17:16 +0800

    enemy class

--- Assets/Editor/CSVtoSO.cs
+++ Assets/Editor/CSVtoSO.cs
@@ -6,6 +6,7 @@ public class CSVtoSO
 {
     private static string playerCSVPath = "/Assets/Editor/CSVs/Player.csv";
     private static string foodCSVPath = "/Assets/Editor/CSVs/Food.csv";
+    private static string enemyCSVPath = "/Assets/Editor/CSVs/Enemy.csv";
 
     [MenuItem("Utilities/Generate Player")]
     public static void GeneratePlayer()
@@ -50,6 +51,31 @@ public class CSVtoSO
             foodItem.currentPoints = foodItem.maxPoints;
             foodItem.weight = int.Parse(splitData[4]);
             AssetDatabase.CreateAsset(foodItem, $"Assets/Resources/Data/Food/{foodItem.id}.asset");
+        }
+        AssetDatabase.SaveAssets();
+    }
+
+    [MenuItem("Utilities/Generate Enemy")]
+    public static void GenerateEnemies()
+    {
+        string[] allLines = File.ReadAllLines(System.IO.Directory.GetCurrentDirectory() + enemyCSVPath);
+        foreach (string s in allLines)
+        {
+            string[] splitData = s.Split(',');
+
+            if (splitData.Length != 6)
+            {
+                return;
+            }
+
+            Enemy enemy = ScriptableObject.CreateInstance<Enemy>();
+            enemy.id = int.Parse(splitData[0]);
+            enemy.enemyName = splitData[1];
+            enemy.walkSpeed = float.Parse(splitData[2]);
+            enemy.runSpeed = float.Parse(splitData[3]);
+            enemy.lineOfSight = float.Parse(splitData[4]);
+            enemy.lineOfSight = float.Parse(splitData[5]);
+            AssetDatabase.CreateAsset(enemy, $"Assets/Resources/Data/Enemy/{enemy.id}.asset");
         }
         AssetDatabase.SaveAssets();
     }
