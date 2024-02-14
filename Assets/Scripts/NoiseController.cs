37c82220e65a0bf97f80a157bfda23f7dc5ebb4d
Kieran Chai <mayork383@gmail.com>
2024-02-15 01:12:00 +0800

    Add multiple burst noise and infinite noise

--- Assets/Scripts/NoiseController.cs
+++ Assets/Scripts/NoiseController.cs
@@ -14,6 +14,7 @@ public class NoiseController : MonoBehaviour
 
     // Dynamic data
     private Vector3 currentNoiseScale;
+    public bool isDeactivated = false;
     #endregion
 
     private void Awake()
@@ -49,15 +50,69 @@ public class NoiseController : MonoBehaviour
         float noiseShrinkLerpSpeed = 9.0f * Time.deltaTime;
         while (noiseShrinkTime < noiseShrinkDuration)
         {
-            noiseSpreadTime += Time.deltaTime;
+            noiseShrinkTime += Time.deltaTime;
             currentNoiseScale = Vector3.Lerp(currentNoiseScale, Vector3.zero, noiseShrinkLerpSpeed);
             _transform.localScale = currentNoiseScale;
             yield return null;
         }
+        _transform.localScale = Vector3.zero;
     }
 
-    public void ProduceNoiseContinuous()
+    public IEnumerator ProduceNoiseMultiple(int numberOfTimes)
     {
+        for (int i = 0; i < numberOfTimes; i++)
+        {
+            float noiseSpreadTime = 0.0f;
+            float noiseSpreadDuration = 0.3f;
+            float noiseSpreadLerpSpeed = 12.0f * Time.deltaTime;
+            while (noiseSpreadTime < noiseSpreadDuration)
+            {
+                noiseSpreadTime += Time.deltaTime;
+                currentNoiseScale = Vector3.Lerp(currentNoiseScale, baseNoiseScale, noiseSpreadLerpSpeed);
+                _transform.localScale = currentNoiseScale;
+                yield return null;
+            }
 
+            float noiseShrinkTime = 0.0f;
+            float noiseShrinkDuration = 0.1f;
+            float noiseShrinkLerpSpeed = 9.0f * Time.deltaTime;
+            while (noiseShrinkTime < noiseShrinkDuration)
+            {
+                noiseShrinkTime += Time.deltaTime;
+                currentNoiseScale = Vector3.Lerp(currentNoiseScale, Vector3.zero, noiseShrinkLerpSpeed);
+                _transform.localScale = currentNoiseScale;
+                yield return null;
+            }
+            _transform.localScale = Vector3.zero;
+        }
+    }
+
+    public IEnumerator ProduceNoiseInfinitely()
+    {
+        while (!isDeactivated)
+        {
+            float noiseSpreadTime = 0.0f;
+            float noiseSpreadDuration = 0.3f;
+            float noiseSpreadLerpSpeed = 12.0f * Time.deltaTime;
+            while (noiseSpreadTime < noiseSpreadDuration)
+            {
+                noiseSpreadTime += Time.deltaTime;
+                currentNoiseScale = Vector3.Lerp(currentNoiseScale, baseNoiseScale, noiseSpreadLerpSpeed);
+                _transform.localScale = currentNoiseScale;
+                yield return null;
+            }
+        }
+
+        float noiseShrinkTime = 0.0f;
+        float noiseShrinkDuration = 0.1f;
+        float noiseShrinkLerpSpeed = 9.0f * Time.deltaTime;
+        while (noiseShrinkTime < noiseShrinkDuration)
+        {
+            noiseShrinkTime += Time.deltaTime;
+            currentNoiseScale = Vector3.Lerp(currentNoiseScale, Vector3.zero, noiseShrinkLerpSpeed);
+            _transform.localScale = currentNoiseScale;
+            yield return null;
+        }
+        _transform.localScale = Vector3.zero;
     }
 }
